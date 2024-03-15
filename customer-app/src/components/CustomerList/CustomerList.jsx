abdd1c6714ddd558c8dd51310c65043774bde766
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-16 02:17:04 +0300

    deneme

--- customer-app/src/components/CustomerList/CustomerList.jsx
+++ customer-app/src/components/CustomerList/CustomerList.jsx
@@ -7,15 +7,17 @@ function CustomerList({ customers, setCustomers }) {
     setCustomers(customers.filter((customer) => customer.id !== item.id));
   };
   return (
-    <ul className="customer-list">
-      {customers.map((customer) => (
-        <CustomerItem
-          customer={customer}
-          key={customer.id}
-          handleDelete={handleDelete}
-        />
-      ))}
-    </ul>
+    <ul className="customer-list" >
+      {
+        customers.map((customer) => (
+          <CustomerItem
+            customer={customer}
+            key={customer.id}
+            handleDelete={handleDelete}
+          />
+        ))
+      }
+    </ul >
   );
 }
 
