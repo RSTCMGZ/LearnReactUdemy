2b23df451047e59390c2bd9e6ced251829b79937
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-21 00:25:27 +0300

    Button-Card-Add

---
+++ workerapp/src/components/UI/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button(props) {
+    return (
+        <button className={`p-2 text-lg bg-teal-700 text-white mt-2 
+        ${props.className}
+        `}>{props.children}</button>
+    )
+}
+
+export default Button
--- workerapp/src/components/Workers/AddWorker.jsx
+++ workerapp/src/components/Workers/AddWorker.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import Card from '../UI/Card'
+import Button from '../UI/Button'
 
 function AddWorker() {
     return (
-        <form className='flex flex-col gap-y-2'>
-            <label htmlFor='name' className='font-medium'>Çalışan İsmi</label>
-            <input type="text" className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Çalışan ismi yazınız' id='name' />
-            <label htmlFor='wage' className='font-medium'>Maaş Miktarı</label>
-            <input type="text" className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Maaş  miktarı giriniz' id='wage' />
-            <button type='submit' className='p-2 text-lg bg-teal-700 text-white mt-2'>Ekle</button>
-        </form>
+        <Card className='mt-10 '>
+            <form className='flex flex-col gap-y-2'>
+                <label htmlFor='name' className='font-medium'>Çalışan İsmi</label>
+                <input type="text" className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Çalışan ismi yazınız' id='name' />
+                <label htmlFor='wage' className='font-medium'>Maaş Miktarı</label>
+                <input type="number" className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Maaş  miktarı giriniz' id='wage' />
+                <Button className='mt-2'>Ekle</Button>
+            </form>
+
+        </Card>
+
     )
 }
 
