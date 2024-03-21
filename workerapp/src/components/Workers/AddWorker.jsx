4904598fd43e1fb174d6204367ac024ea6ecd8dd
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-22 02:02:28 +0300

    Portals,Fragments

--- workerapp/src/components/Workers/AddWorker.jsx
+++ workerapp/src/components/Workers/AddWorker.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddWorker = (props) => {
     const [enteredWorkerName, setEnteredWorkerName] = useState("");
@@ -43,7 +44,7 @@ const AddWorker = (props) => {
         setError(null);
     };
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal onConfirm={errorHandler} error={error} />}
             <Card className="mt-10">
                 <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
@@ -74,7 +75,7 @@ const AddWorker = (props) => {
                     </Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 };
 
