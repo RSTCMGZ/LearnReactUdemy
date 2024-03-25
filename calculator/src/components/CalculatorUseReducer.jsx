852b5eb20792580594e894c5eb9043a93ecd8254
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-26 02:40:06 +0300

    ReducerFinish

--- calculator/src/components/CalculatorUseReducer.jsx
+++ calculator/src/components/CalculatorUseReducer.jsx
@@ -1,29 +1,10 @@
 import { useReducer } from 'react'
 import './Calculator.css'
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'SET_NUMBER1':
-            return { ...state, number1: action.payload }
-        case 'SET_NUMBER2':
-            return { ...state, number2: action.payload }
-        case 'SET_RESULT':
-            return { ...state, result: action.payload }
-        default:
-            throw new Error('Invalid action type!')
-    }
-}
-const initialState = {
-    number1: 0,
-    number2: 0,
-    result: 0
-}
+import { initialState, reducer } from './reducers/ReducerCalculator'
 
 export const CalculatorUseReducer = () => {
 
     const [state, dispatch] = useReducer(reducer, initialState)
-
-
 
     const handleNumber1Change = (e) => {
         dispatch({ type: 'SET_NUMBER1', payload: Number(e.target.value) })
@@ -34,6 +15,8 @@ export const CalculatorUseReducer = () => {
 
     const handleAddition = () => {
         dispatch({ type: 'SET_RESULT', payload: state.number1 + state.number2 })
+        dispatch({ type: 'RESET' });
+
     }
     return (
         <div className="calculator-container">
