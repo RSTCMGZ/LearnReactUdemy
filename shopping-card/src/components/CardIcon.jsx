931aaddef2e88b6f705fc95ef214ee5d1f84ba28
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-29 01:26:12 +0300

    learnContextApi

--- shopping-card/src/components/CardIcon.jsx
+++ shopping-card/src/components/CardIcon.jsx
@@ -1,11 +1,17 @@
 import { FiShoppingCart } from 'react-icons/fi'
-
+import { MyContext } from '../main'
+import { useContext } from 'react'
 function CardIcon({ cart }) {
+
+    const { myState, setMyState } = useContext(MyContext)
+
+
     return (
         <div>
 
             <div className='relative'>
                 <FiShoppingCart className='text-2xl' />
+
                 {
                     cart.length > 0 && (
                         <span className='bg-red-500 text-white  w-5 h-5 flex items-center justify-center rounded-full absolute -top-2 -right-4 text-sm-xs'>
--- shopping-card/src/main.jsx
+++ shopping-card/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+export const MyContext = React.createContext()
+
+const MyProvider = ({ children }) => {
+
+  const [myState, setMyState] = useState('Reso')
+
+  const value = {
+    myState,
+    setMyState
+  }
+
+  return <MyContext.Provider value={value}>
+  </MyContext.Provider>
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MyProvider>
+      <App />
+    </MyProvider>
   </React.StrictMode>,
 )
+
