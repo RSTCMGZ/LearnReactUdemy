84bdde4a14fa3f235f82eb3b6f22ad151774c093
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-04-02 00:46:20 +0300

    hero-finish

--- trend-store/src/App.jsx
+++ trend-store/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero/Hero"
 import Header from "./components/Layout/Header"
 
 function App() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   )
 }
