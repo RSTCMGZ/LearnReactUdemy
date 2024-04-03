39efd1891ccbc07f3860a89a8126561fb5ebd3e6
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-04-04 02:38:02 +0300

    offCanvasUse

--- trend-store/src/components/Cart/Card.jsx
+++ trend-store/src/components/Cart/Card.jsx
@@ -1,4 +1,5 @@
 import products from '../../productData'
+import Offcanvas from '../UI/Offcanvas'
 import './Cart.css'
 import CartItem from './CartItem'
 const Card = (props) => {
@@ -10,25 +11,22 @@ const Card = (props) => {
             )}
         </ul>
     )
-
     return (
-        <div className='offcanvas'>
-            <div className="content">
-                <div className="cart-head">
-                    <h2>Sepetim</h2>
-                    <a href="/" onClick={props.onClose} className='cart-close'>X</a>
-                </div>
-                {cartItems}
-                <div className="total">
-                    <span>Toplam Değer</span>
-                    <span>10₺</span>
-                </div>
-                <div className="actions">
-                    <button className='cart-border'>Sipariş ver</button>
-                    <button className='cart-clear'>Temizle</button>
-                </div>
+        <Offcanvas onClose={props.onClose}>
+            <div className="cart-head">
+                <h2>Sepetim</h2>
+                <a href="/" onClick={props.onClose} className='cart-close'>X</a>
             </div>
-        </div>
+            {cartItems}
+            <div className="total">
+                <span>Toplam Değer</span>
+                <span>10₺</span>
+            </div>
+            <div className="actions">
+                <button className='cart-border'>Sipariş ver</button>
+                <button className='cart-clear'>Temizle</button>
+            </div>
+        </Offcanvas>
     )
 }
 
