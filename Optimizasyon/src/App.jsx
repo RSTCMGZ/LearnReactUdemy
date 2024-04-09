b42d444e292eaddf278c18ad52b6ed9127366d43
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-04-09 23:45:42 +0300

    lessons-again

--- Optimizasyon/src/App.jsx
+++ Optimizasyon/src/App.jsx
@@ -1,11 +1,17 @@
-
+import { useState } from 'react'
 import './App.css'
+import Button from './components/UI/Button'
 
 function App() {
-
+  const [toggleParagraph, setToggleParagraph] = useState(false)
+  const toggleParagraphHandler = () => {
+    setToggleParagraph(true)
+  }
   return (
     <div className='container'>
       <h1>Hello</h1>
+      <b>{toggleParagraph ? 'true' : 'false'}</b> <br /> <br />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   )
 }
