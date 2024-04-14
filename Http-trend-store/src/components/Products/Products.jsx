44c375729384b2ea47ecd5905272ff7e86f8e5ef
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-04-14 17:50:35 +0300

    isLoadingState

--- Http-trend-store/src/components/Products/Products.jsx
+++ Http-trend-store/src/components/Products/Products.jsx
@@ -4,31 +4,39 @@ import "./Products.css";
 
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+
   const productList = products.map((product) => (
     <ProductItem key={product.id} product={product} />
   ));
 
   const fetchProductsHandler = async () => {
-    fetch('https://my-pos-application-api.onrender.com/api/products/get-all')
-    return response.json()
-
+    setIsLoading(true)
+    const response = await fetch('https://my-pos-application-api.onrender.com/api/products/get-all')
+    const data = await response.json()
 
     const newData = data.map((item) => {
       return {
         id: item._id,
         name: item.title,
-        ...item
-      }
-    })
+        ...item,
+      };
+    });
     setProducts(newData)
+    setIsLoading(false)
+
   }
-}
-return (
-  <main className="products-wrapper">
-    <ul className="products">{productList}</ul>
-    <button className="button" onClick={fetchProductsHandler}>Fetch Products</button>
-  </main>
-);
+
+  return (
+    <main className="products-wrapper">
+      <ul className="products">{!isLoading && productList}</ul>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && products.length === 0 && <p>nothing products!</p>}
+      <button className="button"
+        onClick={fetchProductsHandler}>Fetch Products</button>
+    </main>
+  );
 };
 
 export default Products;
