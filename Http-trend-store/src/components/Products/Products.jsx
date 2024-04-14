077168f91f64eb332e48ecf894566381f4423d46
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-04-14 18:14:21 +0300

    error-Handling

--- Http-trend-store/src/components/Products/Products.jsx
+++ Http-trend-store/src/components/Products/Products.jsx
@@ -5,6 +5,7 @@ import "./Products.css";
 const Products = () => {
   const [products, setProducts] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
 
   const productList = products.map((product) => (
@@ -13,26 +14,42 @@ const Products = () => {
 
   const fetchProductsHandler = async () => {
     setIsLoading(true)
-    const response = await fetch('https://my-pos-application-api.onrender.com/api/products/get-all')
-    const data = await response.json()
+    setError(null)
+    try {
+      const response = await fetch('https://my-pos-application-api.onrender.com/api/products/get-all')
+      if (response.status !== 200) {
+        throw new Error('Something went wrong!')
+      }
+      const data = await response.json()
 
-    const newData = data.map((item) => {
-      return {
-        id: item._id,
-        name: item.title,
-        ...item,
-      };
-    });
-    setProducts(newData)
+      const newData = data.map((item) => {
+        return {
+          id: item._id,
+          name: item.title,
+          ...item,
+        };
+      });
+      setProducts(newData)
+    } catch (error) {
+      setError(error.message)
+    }
     setIsLoading(false)
-
+  }
+  let content = <p>nothing products!</p>
+  if (products.length > 0) {
+    content = productList
+  }
+  if (error) {
+    content = <p>{error}</p>
   }
 
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
   return (
     <main className="products-wrapper">
-      <ul className="products">{!isLoading && productList}</ul>
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && products.length === 0 && <p>nothing products!</p>}
+      <ul className="products">{content}</ul>
+
       <button className="button"
         onClick={fetchProductsHandler}>Fetch Products</button>
     </main>
