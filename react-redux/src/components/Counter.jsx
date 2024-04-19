781ff3c381a25a68440bc782440fd2ad4792a96c
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-04-20 00:50:28 +0300

    useSellectorUseDispatch

--- react-redux/src/components/Counter.jsx
+++ react-redux/src/components/Counter.jsx
@@ -1,10 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+  const incrementHandler = () => {
+    dispatch({ type: 'INCREMENT' })
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'DECREMENT' })
+
+  };
+  const toggleCounterHandler = () => { };
 
   return (
     <main>
       <h1>Redux Counter</h1>
-      <b> 0 </b> <br />
+      <b> {counter} </b> <br />
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button> <br />
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
