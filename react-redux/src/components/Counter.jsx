a77093a63ba5c2171557e6b10310264c4a382a0f
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-04-20 01:07:13 +0300

    toggle

--- react-redux/src/components/Counter.jsx
+++ react-redux/src/components/Counter.jsx
@@ -3,21 +3,29 @@ import { useDispatch, useSelector } from 'react-redux'
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter)
+  const showCounter = useSelector((state) => state.showCounter)
   const dispatch = useDispatch()
+
   const incrementHandler = () => {
     dispatch({ type: 'INCREMENT' })
+  };
+  const increaseHandler = () => {
+    dispatch({ type: 'INCREASE', amount: 5 })
   };
   const decrementHandler = () => {
     dispatch({ type: 'DECREMENT' })
 
   };
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE' })
+  };
 
   return (
     <main>
       <h1>Redux Counter</h1>
-      <b> {counter} </b> <br />
+      <b> {showCounter && counter} </b> <br />
       <button onClick={incrementHandler}>Increment</button>
+      <button onClick={increaseHandler}>Increase by 5</button>
       <button onClick={decrementHandler}>Decrement</button> <br />
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
